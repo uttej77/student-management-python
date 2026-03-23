7841b7bd276ab324b08c18871df6ea69b47ce2e4
uttej77 <94373983+uttej77@users.noreply.github.com>
2026-03-23 16:48:43 +0530

    Add files via upload

--- main.py
+++ main.py
@@ -1,18 +1,22 @@
-from students import add_student, view_students, delete_student
+from services.student_service import *
 
-while True:
+def menu():
     print("\n===== Student Management System =====")
     print("1. Add Student")
     print("2. View Students")
     print("3. Delete Student")
-    print("4. Exit")
+    print("4. Update Student")
+    print("5. Search Student")
+    print("6. Exit")
 
+while True:
+    menu()
     choice = input("Enter your choice: ")
 
     if choice == "1":
-        name = input("Enter student name: ")
-        age = input("Enter age: ")
-        course = input("Enter course: ")
+        name = input("Name: ")
+        age = input("Age: ")
+        course = input("Course: ")
         add_student(name, age, course)
 
     elif choice == "2":
@@ -20,10 +24,23 @@ while True:
 
     elif choice == "3":
         view_students()
-        num = int(input("Enter student number to delete: "))
+        num = int(input("Enter number to delete: "))
         delete_student(num - 1)
 
     elif choice == "4":
+        view_students()
+        num = int(input("Enter number to update: "))
+        name = input("New name: ")
+        age = input("New age: ")
+        course = input("New course: ")
+        update_student(num - 1, name, age, course)
+
+    elif choice == "5":
+        keyword = input("Enter name to search: ")
+        search_student(keyword)
+
+    elif choice == "6":
+        print("Goodbye 👋")
         break
 
     else:
